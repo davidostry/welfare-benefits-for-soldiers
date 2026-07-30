e2e8273a45b6341b87e41bb6a56d35cdd3ece764
david <sarigoo421@gmail.com.il>
2026-07-30 10:34:25 +0300

    validator for body created

--- ROUTS/walfareRecordRouter.js
+++ ROUTS/walfareRecordRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { createRecord, getRecord, updateRecord } from '../CTRLS/walfareRecordCtrl.js'
+import {validateBody} from '../MIDDLEWARE/validations.js'
 
 const router = express.Router();
 
-router.post("/:soldierId/benefits", createRecord);
+router.post("/:soldierId/benefits", validateBody, createRecord);
 
 router.get("/:soldierId/benefits", getRecord);
 
