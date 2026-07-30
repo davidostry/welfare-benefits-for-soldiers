9154c75e0cfb3de80d5d95d0d15e88613c9a9d67
david <sarigoo421@gmail.com.il>
2026-07-30 09:44:07 +0300

    added business logic for first endpoint

--- SERVICE/walfareRecordService.js
+++ SERVICE/walfareRecordService.js
@@ -1,10 +1,29 @@
 import { createRecord, getRecordById, updateRecord } from '../DAL/walfareRecordRepo.js'
 
 export async function addRecord(id, record) {
-    const check = await getRecordById(id)
-    if (check) throw new Error("record is already exists");
-    
-    return await createRecord(record)
+    // const soldier = await getRecordById(id)
+    // if (soldier) {
+    //     const error = new Error("record is already exists");
+    //     error.status(209)
+    //     return error
+    // }
+    const newRecord = {
+        soldierId: id,
+        unit: record.unit,
+        currentBenefitTAype: record.benefitTAype,
+        history: [
+            {
+                startDate: record.startDate || new Date,
+                endDate: null,
+                decisionReason: record.decisionReason,
+                budgetApprove: record.budgetApprove,
+                benefitTAype: record.benefitTAype,
+                details: record.details
+            }
+        ]
+    }
+
+    return await createRecord(newRecord)
 }
 
 export async function showRecord(id) {
@@ -12,6 +31,6 @@ export async function showRecord(id) {
     return await getRecordById(id)
 }
 
-export async function editRecord(id, record){
+export async function editRecord(id, record) {
     return await updateRecord(id, record)
 }
